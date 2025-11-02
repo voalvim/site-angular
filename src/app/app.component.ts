36417edd495fe062d836f98c762d0864aae89218
Vitor Ornelas Alvim <ornelas.dev@gmail.com>
2025-11-02 01:03:08 -0300

    create initial application structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'site-angular';
+  readonly gfName = 'Helena';
+  readonly startDate = new Date("2025-01-26T10:00:00");
+
+  intervalId?: number;
+
+
+  ngOnInit() {
+     this.intervalId = setInterval(() => {
+      console.log(new Date());
+    }, 
+    1000); 
+  } 
+
+  ngOnDestroy() {
+    if (this.intervalId) clearInterval(this.intervalId);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt-PT';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizeDatePipe } from './pipes/capitalize-date.pipe';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizeDatePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-PT' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
